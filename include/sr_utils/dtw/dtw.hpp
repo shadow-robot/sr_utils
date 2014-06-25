db624725e3e84b3f8b96017116b362604079d2f7
Ugo Cupcic <ugo@shadowrobot.com>
2014-06-25 14:29:41 +0200

    fixing license

--- include/sr_utils/dtw/dtw.hpp
+++ include/sr_utils/dtw/dtw.hpp
@@ -2,11 +2,20 @@
  * @file   dtw.hpp
  * @author Yi Li <yi@shadowrobot.com>
  *
- * Copyright (c) 2014 Shadow Robot Company Ltd.
- *  All rights reserved.
+ * Copyright (c) 2013, Shadow Robot Company, All rights reserved.
  *
- * This code is proprietary and may not be used, copied, distributed without
- *  prior authorisation and agreement from Shadow Robot Company Ltd.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
  *
  * @brief An implementation of dynamic time warping (DTW).
  *
